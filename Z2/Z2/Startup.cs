5963370f56ee5cfd0749b942e6f468c095074b38
Drimve <86664972+Drimve@users.noreply.github.com>
2021-11-05 13:07:08 +0500

    upload files
    -

---
+++ Z2/Z2/Startup.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Z2
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IFibonachiN, FibonachiN>();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<CustomMiddleware>();
+        }
+    }
+}
+
+
